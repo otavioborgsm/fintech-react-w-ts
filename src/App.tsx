912ffacdd242c9cf7de39fca96c2742a7c64b536
otavioborgsm <otavio.demb@rede.ulbra.br>
2026-05-14 18:16:15 -0300

    0402: - Create page and import it; - Create components and import them; - Copy useFetch custom hook from "React with Typescript" course

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,17 @@
+import Header from './Components/Header'
+import Sidenav from './Components/Sidenav'
+import Resumo from './Pages/Resumo'
 import './Style.css'
 
 function App() {
 
   return (
     <>
+      <Sidenav />
+      <main>
+        <Header />
+        <Resumo />
+      </main>
     </>
   )
 }
