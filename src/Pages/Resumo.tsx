3f1dc3fd76d2e10f0b3a0a412b6f5625e9b1a77c
otavioborgsm <otavio.demb@rede.ulbra.br>
2026-05-20 16:15:01 -0300

    0409: Create Sales summary section

--- src/Pages/Resumo.tsx
+++ src/Pages/Resumo.tsx
@@ -3,8 +3,46 @@ import { useData } from '../Context/DataContext';
 
 const Resumo = () => {
   const { data } = useData();
+
+  if (data === null) return null;
   return (
-    <div>Resumo</div>
+    <section>
+      <div className='resumo flex mb'>
+        <div className='box'>
+          <h2>Vendas</h2>
+          <span>
+            {data
+              .filter((i) => i.status !== 'falha')
+              .reduce((acc, item) => acc + item.preco, 0)
+              .toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' })}
+          </span>
+        </div>
+
+        <div className='box'>
+          <h2>Recebido</h2>
+          <span>
+            {data
+              .filter((i) => i.status === 'pago')
+              .reduce((acc, item) => acc + item.preco, 0)
+              .toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' })}
+          </span>
+        </div>
+
+        <div className='box'>
+          <h2>Processando</h2>
+          <span>
+            {data
+              .filter((i) => i.status === 'processando')
+              .reduce((acc, item) => acc + item.preco, 0)
+              .toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' })}
+          </span>
+        </div>
+      </div>
+
+      <div className='box'>
+            Gráficos
+      </div>
+    </section>
   )
 }
 
